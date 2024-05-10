98406cc50d10eca86e64ed66db41963e53f15088
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-09 21:11:50 -0300

    01_09: read inputs and display array of numbers on HTML

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,8 +1,32 @@
-let quantidade;
-let de;
-let ate;
+function sortear() {
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+  let de = parseInt(document.getElementById("de").value);
+  let ate = parseInt(document.getElementById("ate").value);
 
-// let botaoSorteio = document.getElementById();
+  let sorteados = [];
+  let numero;
+
+  if (isValid(quantidade, de, ate)) {
+    console.log(`Irei sortear ${quantidade} números.`);
+    for (let i = 0; i < quantidade; i++) {
+      numero = obterNumeroAleatorio(de, ate);
+
+      while (sorteados.includes(numero)) {
+        numero = obterNumeroAleatorio(de, ate);
+      }
+
+      sorteados.push(numero);
+    }
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+  } else {
+    console.log("Valor inválido!");
+  }
+}
+
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function isValid(quantidade, de, ate) {
   let error =
@@ -17,19 +41,4 @@ function isValid(quantidade, de, ate) {
     isNaN(ate);
 
   return !error;
-}
-
-function sortear() {
-  quantidade = parseInt(document.getElementById("quantidade").value);
-  de = parseInt(document.getElementById("de").value);
-  ate = parseInt(document.getElementById("ate").value);
-
-  if (isValid(quantidade, de, ate)) {
-    console.log(`Irei sortear ${quantidade} números.`);
-    for (let i = 0; i < quantidade; i++) {
-      console.log(parseInt(Math.random() * ate + 1));
-    }
-  } else {
-    console.log("Valor inválido!");
-  }
 }
