d8e6a49892e7514de2b4405d72683b6c3cba30c2
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-13 20:03:39 -0300

    refactor to use single quotes

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,21 +1,21 @@
 function sortear() {
-  let quantidade = parseInt(document.getElementById("quantidade").value);
-  let de = parseInt(document.getElementById("de").value);
-  let ate = parseInt(document.getElementById("ate").value);
+  let quantidade = parseInt(document.getElementById('quantidade').value);
+  let de = parseInt(document.getElementById('de').value);
+  let ate = parseInt(document.getElementById('ate').value);
 
   let sorteados = [];
   let numero;
 
   if (isValid(quantidade, de, ate)) {
     if (de > ate) {
-      alert("Número inicial não pode ser maior que número final!");
+      alert('Número inicial não pode ser maior que número final!');
       return;
     }
 
     let intervalo = ate - de + 1;
     if (quantidade > intervalo) {
       alert(
-        "É preciso que o intervalo seja maior ou igual à quantidade de números!"
+        'É preciso que o intervalo seja maior ou igual à quantidade de números!'
       );
     } else {
       console.log(`Irei sortear ${quantidade} números.`);
@@ -28,30 +28,30 @@ function sortear() {
 
         sorteados.push(numero);
       }
-      let resultado = document.getElementById("resultado");
+      let resultado = document.getElementById('resultado');
       resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
       alternarStatusBotaoReiniciar();
     }
   } else {
-    console.log("Campo vazio!");
+    console.log('Campo vazio!');
   }
 }
 
 function reiniciar() {
-  document.getElementById("quantidade").value = "";
-  document.getElementById("de").value = "";
-  document.getElementById("ate").value = "";
+  document.getElementById('quantidade').value = '';
+  document.getElementById('de').value = '';
+  document.getElementById('ate').value = '';
   alternarStatusBotaoReiniciar();
 }
 
 function alternarStatusBotaoReiniciar() {
-  let botao = document.getElementById("btn-reiniciar");
-  if (botao.classList.contains("container__botao-desabilitado")) {
-    botao.classList.remove("container__botao-desabilitado");
-    botao.classList.add("container__botao");
+  let botao = document.getElementById('btn-reiniciar');
+  if (botao.classList.contains('container__botao-desabilitado')) {
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
   } else {
-    botao.classList.add("container__botao-desabilitado");
-    botao.classList.remove("container__botao");
+    botao.classList.add('container__botao-desabilitado');
+    botao.classList.remove('container__botao');
   }
 }
 
@@ -61,13 +61,13 @@ function obterNumeroAleatorio(min, max) {
 
 function isValid(quantidade, de, ate) {
   let error =
-    quantidade === "" ||
+    quantidade === '' ||
     quantidade === undefined ||
     isNaN(quantidade) ||
-    de === "" ||
+    de === '' ||
     de === undefined ||
     isNaN(de) ||
-    ate === "" ||
+    ate === '' ||
     ate === undefined ||
     isNaN(ate);
 
