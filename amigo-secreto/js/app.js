b2d483f32782533a78379afcf10e892fa220d3e0
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-14 15:55:35 -0300

    05_01: implement all functions. Working properly

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -9,6 +9,15 @@ function adicionar() {
 
 function sortear() {
   if (amigos.length % 2 == 0) {
+    let ordem = randperm(0, amigos.length - 1);
+    let resultado = document.getElementById('lista-sorteio');
+    resultado.textContent = '';
+
+    for (let i = 0; i < amigos.length; i += 2) {
+      resultado.textContent += `${amigos[ordem[i]]} -> ${
+        amigos[ordem[i + 1]]
+      }\n\n`;
+    }
   } else {
     alert('É preciso um número par de amigos!');
   }
@@ -18,6 +27,7 @@ function reiniciar() {
   amigos = [];
   document.getElementById('nome-amigo').value = '';
   document.getElementById('lista-amigos').textContent = '';
+  document.getElementById('lista-sorteio').textContent = '';
 }
 
 function randperm(min, max) {
